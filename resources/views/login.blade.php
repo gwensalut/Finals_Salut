dd436ef59bf91ed87faff118fd86d24188c630f2
gwensalut <131321598+gwensalut@users.noreply.github.com>
2023-06-22 10:59:17 +0800

    DONE
    DONE

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <h1>WELCOME TO LOG IN PAGE</h1>
+    <h2>ACTIVITY PLUS 20 & COLLOQUIUM ATTENDEES</h2>
     <form action="{{ route ('login.submit')}}" method="post">
         @csrf
         <div>
